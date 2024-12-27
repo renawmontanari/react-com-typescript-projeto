1714c170ee996ba9db04b47db2de21b130abf5b0
renawmontanari <regiangelica@gmail.com>
2024-12-26 22:24:33 -0300

    iniciando o Todo

---
+++ todo_react_ts/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Components
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Header />
+        <h1>Conteúdo..</h1>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo_react_ts/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header clasName={styles.header}>
+      <h1>Todo React com Typescript</h1>
+    </header>
+  );
+};
+
+export default Header;
